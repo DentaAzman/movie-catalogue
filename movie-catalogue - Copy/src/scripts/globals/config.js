9fe6645ac4a65837ef154998e39b41b2c7ed6c62
DentaAzman <152490890+DentaAzman@users.noreply.github.com>
2024-04-24 14:13:15 +0700

    Add files via upload

---
+++ movie-catalogue - Copy/src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: 'f2e21b279c94d20cd47f663e78fc5340',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
